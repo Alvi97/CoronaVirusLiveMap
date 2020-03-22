9f009f4d8cf96d3b0ac87e6b03596734d4182e2d
Alvi Lika <alvi.lika@moreone.tech>
2020-03-22 20:50:32 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {Apis} from './services/api.service'
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [Apis], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,67 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+@Injectable()
+
+
+export class Apis{
+
+
+
+constructor(private http:HttpClient) {}
+
+httpOptions = {
+    headers: new HttpHeaders({
+      'x-rapidapi-host':  'coronavirus-monitor.p.rapidapi.com',
+      'x-rapidapi-key': '5ac3fafademsh1a8ef1b0741135dp158e82jsn6c5887e81b38'
+    })
+  };
+
+Countries() : Observable<any>{
+
+const url = 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/affected.php'
+
+   
+
+     return this.http.get(url , this.httpOptions)
+
+}
+
+TotalCases() : Observable<any>{
+
+    const url = 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php'
+    
+       
+    
+         return this.http.get(url , this.httpOptions)
+    
+    }
+
+
+    TotalCasesByCountry() : Observable<any>{
+
+        const url = 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php'
+        
+           
+        
+             return this.http.get(url , this.httpOptions)
+        
+        }
+
+        TotalCasesBySpecificCountry() : Observable<any>{
+
+
+
+            const url = 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_particular_country.php?country=Germany'
+            
+               
+            
+                 return this.http.get(url , this.httpOptions)
+            
+            }
+    
+
+}
+
